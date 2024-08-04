6656e7163276cb2ac8c4fc0d24fa3ec44a870040
Буянов Всеволод <mors@nemors.ru>
2024-08-05 01:35:00 +0300

    added

--- certbot/generate-certificate.sh
+++ certbot/generate-certificate.sh
@@ -1,4 +1,4 @@
-rm -rf /etc/letsencrypt/live/gw
+rm -rf /etc/letsencrypt/live/gw*
 
 # выдаем себе сертификат (обратите внимание на переменные среды)
 certbot certonly --standalone -d $DOMAIN_URL --cert-name=certfolder --key-type rsa --agree-tos
@@ -9,5 +9,5 @@ rm -rf /etc/nginx/cert.pem
 rm -rf /etc/nginx/key.pem
 
 # копируем сертификаты из образа certbot в папку Nginx
-cp /etc/letsencrypt/live/gw/fullchain.pem /etc/nginx/cert.pem
-cp /etc/letsencrypt/live/gw/privkey.pem /etc/nginx/key.pem
+cp /etc/letsencrypt/live/gw*/fullchain.pem /etc/nginx/cert.pem
+cp /etc/letsencrypt/live/gw*/privkey.pem /etc/nginx/key.pem
