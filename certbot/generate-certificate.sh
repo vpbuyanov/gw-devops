e9dcfb5894446946ae9f809cce2cf87cfca626f7
Буянов Всеволод <mors@nemors.ru>
2024-08-05 21:16:07 +0300

    added

--- certbot/generate-certificate.sh
+++ certbot/generate-certificate.sh
@@ -2,3 +2,10 @@ rm -rf /etc/letsencrypt/live/vpbuyanov.ru
 
 # выдаем себе сертификат
 certbot certonly --standalone --email $DOMAIN_EMAIL -d $DOMAIN_URL --cert-name=vpbuyanov.ru --key-type rsa --agree-tos
+
+rm -rf /etc/nginx/cert.pem
+rm -rf /etc/nginx/key.pem
+
+# копируем сертификаты из образа certbot в папку Nginx
+cp /etc/letsencrypt/live/vpbuyanov.ru/fullchain.pem /etc/nginx/cert.pem
+cp /etc/letsencrypt/live/vpbuyanov.ru/privkey.pem /etc/nginx/key.pem
